43d6ca3b157715876242655f2615d68dd58ad95c
Antara <antaramimi@gmail.com>
2020-06-18 19:12:41 +0100

    Adding more test

--- src/main/java/com/antara/bankingsystem/BankingSystem.java
+++ src/main/java/com/antara/bankingsystem/BankingSystem.java
@@ -1,11 +1,10 @@
 package com.antara.bankingsystem;
 
-
 import java.util.*;
 
 public class BankingSystem {
 
-    Map<String, Integer> map = new HashMap<>();
+    Map<User, Integer> map = new HashMap<>();
 
     public static void main(String[] args) {
         BankingSystem bs = new BankingSystem();
@@ -30,54 +29,53 @@ public class BankingSystem {
             switch (option) {
 
                 case 1:
-
                     System.out.println("Enter the customer name");
                     String customerName = sc.next();
+                    User user = new User(customerName);
                     System.out.println("Enter an amount you want to deposit");
                     int amount = sc.nextInt();
-                    if (map.containsKey(customerName)) {
-                        map.put(customerName, map.get(customerName) + amount);
+                    if (map.containsKey(user)) {
+                        map.put(user, map.get(user) + amount);
                     } else {
-                        map.put(customerName, amount);
+                        map.put(user, amount);
                     }
                     System.out.println("Thank you");
                     break;
                 case 2:
                     System.out.println("Enter the customer name");
                     String customerUserName = sc.next();
-                    System.out.println("Balance = " + map.get(customerUserName));
-
+                    User user5 = new User(customerUserName);
+                    System.out.println("Balance = " + map.get(user5));
                     break;
                 case 3:
                     System.out.println("Enter the customer name");
-                    String customerUserNames = sc.next();
+                    customerName = sc.next();
+                    User user1 = new User(customerName);
                     System.out.println("Enter the amount you like to withdraw");
                     int amounts = sc.nextInt();
-                    if (map.containsKey(customerUserNames)) {
-                        map.put(customerUserNames, map.get(customerUserNames) - amounts);
+                    if (map.containsKey(user1)) {
+                        map.put(user1, map.get(user1) - amounts);
                     } else {
                         System.out.println("No balance");
                     }
                     break;
-
                 case 4:
                     System.out.println("Enter your customer name");
-                    String customerName1 = sc.next();
+                    customerName = sc.next();
+                    User user3 = new User(customerName);
                     System.out.println("Enter the customer name you can to transfer");
                     String customerName2 = sc.next();
+                    User user4 = new User(customerName2);
                     System.out.println("Enter the amount you want to transfer");
                     int amountTransfer = sc.nextInt();
-                    if (map.containsKey(customerName2)) {
-                        map.put(customerName2, map.get(customerName2) + amountTransfer);
-                        map.put(customerName1, map.get(customerName1) - amountTransfer);
-
+                    if (map.containsKey(user4)) {
+                        map.put(user4, map.get(user4) + amountTransfer);
+                        map.put(user3, map.get(user3) - amountTransfer);
                     } else {
                         System.out.println("Sorry customer doesn't exists");
                     }
                     break;
             }
-
         }
-
     }
 }
--- src/main/java/com/antara/bankingsystem/User.java
+++ src/main/java/com/antara/bankingsystem/User.java
@@ -1,9 +1,25 @@
 package com.antara.bankingsystem;
 
+import java.util.Objects;
+
 public class User {
 
     String customerName;
     int customerId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return customerId == user.customerId &&
+                customerName.equals(user.customerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerName, customerId);
+    }
 
     public User(String customerName) {
         this.customerName = customerName;
